9a3eeb28e22643a1dbd1e15ca54d01583ba379a4
budiryan <bryanaa@connect.ust.hk>
2018-02-15 01:26:40 +0800

    disable django browsable api

--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,12 @@
 # todos/views.py
 from rest_framework import generics
+from rest_framework import renderers
 
 from . import models
 from . import serializers
 
 
 class Substance(generics.ListCreateAPIView):
+    renderer_classes = [renderers.JSONRenderer]
     queryset = models.Substance.objects.all()
     serializer_class = serializers.SubstanceSerializer
