02f80d6975b869bbb7e528a99c96bf22b9d8409f
budiryan <bryanaa@connect.ust.hk>
2018-02-22 17:20:24 +0800

    improve the looks of the chart

--- frontend/urinalysis/app/src/main/java/com/example/urinalysis/urinalysis/MainActivity.java
+++ frontend/urinalysis/app/src/main/java/com/example/urinalysis/urinalysis/MainActivity.java
@@ -1,32 +1,14 @@
 package com.example.urinalysis.urinalysis;
 
 import android.support.design.widget.TabLayout;
-
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.TableLayout;
-
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LineChart chart;
-    private Float[] yAxis;
-    private String[] xAxis;
     private static final String TAG = "MainActivity";
 
     private SectionPageAdapter mSectionPageAdapter;
@@ -35,13 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
-
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            drawChart();
-        }
-    };
 
     private void setupViewPager(ViewPager viewPager){
         SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
@@ -69,76 +44,5 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle("FYP Urinalysis");
 
-
-
-//        getAvgPerDayData();
-//        Runnable r = new Runnable() {
-//            @Override
-//            public void run() {
-//                getAvgPerDayData();
-//                handler.sendEmptyMessage(0);
-//            }
-//        };
-//        Thread getAvgDataThread = new Thread(r);
-//        getAvgDataThread.start();
-
-    }
-
-    private void getAvgPerDayData(){
-            // Get the averages data from Backend
-            final JSONObject json = RemoteFetch.getAvgPerDay(14, "glucose");
-
-            if(json == null)
-                Log.d("tag", "json is null");
-            else {
-                    try {
-                        // Parse Dates
-                        JSONArray tempDates = json.getJSONArray("dates");
-                        int lengthDates = tempDates.length();
-                        if (lengthDates > 0) {
-                            xAxis = new String[lengthDates];
-                            for (int i = 0; i < lengthDates; i++) {
-                                xAxis[i] = tempDates.getString(i);
-                            }
-                        }
-
-                        // Parse the values
-                        JSONArray tempValues = json.getJSONArray("values");
-                        int lengthValues = tempValues.length();
-                        if (lengthValues > 0) {
-                            yAxis = new Float[lengthValues];
-                            for (int i = 0; i < lengthValues; i++) {
-                                yAxis[i] = Float.parseFloat(tempValues.getString(i));
-                            }
-                        }
-                    }
-                    catch(Exception e){
-                        Log.e("tag", "Handler Main Activity error", e);
-                    }
-
-            }
-
-    }
-
-    public void drawChart(){
-        chart = findViewById(R.id.chart);
-
-        List<Entry> entries;
-
-        entries = new ArrayList<Entry>();
-
-        for(int i = 0 ; i < xAxis.length ; i++)
-            entries.add(new Entry(i, yAxis[i]));
-
-        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
-
-        LineData lineData = new LineData(dataSet);
-
-        chart.setData(lineData);
-
-        chart.getAxisLeft().setDrawGridLines(false);
-        chart.getXAxis().setDrawGridLines(false);
-
-        chart.invalidate();
     }
 }
--- frontend/urinalysis/app/src/main/java/com/example/urinalysis/urinalysis/OverviewFragment.java
+++ frontend/urinalysis/app/src/main/java/com/example/urinalysis/urinalysis/OverviewFragment.java
@@ -1,11 +1,29 @@
 package com.example.urinalysis.urinalysis;
 
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by budiryan on 2/21/18.
@@ -14,11 +32,109 @@ import android.view.ViewGroup;
 public class OverviewFragment extends Fragment {
     private static final String TAG = "OverviewFragment";
 
+    private LineChart chart;
+    private Float[] yAxis;
+    private String[] xAxis;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            drawChart();
+        }
+    };
+
+    public void drawChart(){
+        chart = getView().findViewById(R.id.chart);
+        chart.setNoDataText("");
+
+        List<Entry> entries;
+
+        entries = new ArrayList<Entry>();
+
+        for(int i = 0 ; i < xAxis.length ; i++)
+            entries.add(new Entry(i, yAxis[i]));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+
+        LineData lineData = new LineData(dataSet);
+//        LineData lineData = new LineData(generateLineDataSet(yVals, ContextCompat.getColor(getContext(), R.color.urinalysis_pink)));
+
+        chart.setData(lineData);
+
+        final XAxis xAxis = chart.getXAxis();
+        xAxis.setDrawGridLines(false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(ContextCompat.getColor(getContext(), R.color.urinalysis_text_light));
+        xAxis.setAvoidFirstLastClipping(true);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setTextColor(ContextCompat.getColor(getContext(), R.color.urinalysis_text_light));
+        leftAxis.disableGridDashedLine();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawLimitLinesBehindData(true);
+
+        chart.getAxisRight().setEnabled(false);
+        chart.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        chart.setGridBackgroundColor(Color.parseColor("#FFFFFF"));
+
+
+        chart.invalidate();
+    }
+
+    private void getAvgPerDayData(){
+        // Get the averages data from Backend
+        final JSONObject json = RemoteFetch.getAvgPerDay(14, "glucose");
+
+        if(json == null)
+            Log.d("tag", "json is null");
+        else {
+            try {
+                // Parse Dates
+                JSONArray tempDates = json.getJSONArray("dates");
+                int lengthDates = tempDates.length();
+                if (lengthDates > 0) {
+                    xAxis = new String[lengthDates];
+                    for (int i = 0; i < lengthDates; i++) {
+                        xAxis[i] = tempDates.getString(i);
+                    }
+                }
+
+                // Parse the values
+                JSONArray tempValues = json.getJSONArray("values");
+                int lengthValues = tempValues.length();
+                if (lengthValues > 0) {
+                    yAxis = new Float[lengthValues];
+                    for (int i = 0; i < lengthValues; i++) {
+                        yAxis[i] = Float.parseFloat(tempValues.getString(i));
+                    }
+                }
+            }
+            catch(Exception e){
+                Log.e("tag", "Handler Main Activity error", e);
+            }
+
+        }
+
+    }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.overview_fragment, container, false);
+
+        getAvgPerDayData();
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                getAvgPerDayData();
+                handler.sendEmptyMessage(0);
+            }
+        };
+        Thread getAvgDataThread = new Thread(r);
+
+        getAvgDataThread.start();
         return view;
     }
 }
