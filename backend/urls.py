2194777cd1a66c5e3c1ead30182b25812869c29f
budiryan <bryanaa@connect.ust.hk>
2018-02-22 17:31:14 +0800

    fix url for category

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('getavgperday', views.GetAvgPerDayView.as_view()),
     path('category', views.CategoryView.as_view()),
-    path('category/<int:id>/', views.CategoryInstanceView.as_view()),
+    path('category/<int:pk>/', views.CategoryInstanceView.as_view()),
     path('substance', views.SubstanceView.as_view()),
     path('substance/<int:pk>/', views.SubstanceInstanceView.as_view()),
     path('unit/<int:pk>/', views.UnitInstanceView.as_view()),
